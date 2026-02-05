83e7e3db3cec98c44edc59ddf91573883a0b8354
Nattha <gamenattha555@gmail.com>
2026-02-05 19:09:51 +0700

    4th fix pks work

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
@@ -6,6 +7,10 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+    super({ adapter: pool });
+  }
   async onModuleInit() {
     await this.$connect();
   }
