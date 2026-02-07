7633f01bf8db12205a3270931a2556325b4e0af1
Nattha <gamenattha555@gmail.com>
2026-02-08 00:09:32 +0700

    add Cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
